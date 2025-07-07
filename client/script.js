cceb2de8fa9b21591a9c49c81dba09bc43d6772a
nowshad-islam-dev <mdnowshadislam.2108033.ruet.mte@gmail.com>
2025-07-08 00:26:52 +0600

    feat: implement scroll animations for hero button and card reveal on scroll

--- client/script.js
+++ client/script.js
@@ -1,5 +1,22 @@
-$(document).ready(function(){
-    $(".nav-link").click(function() {
-        $(this).css("color", "green")
-    })
-})
+$(document).ready(function () {
+  // Scroll to hero button
+  $('.hero-btn').on('click', function () {
+    $('html, body').animate({
+      scrollTop: $('.features').offset().top
+    }, 1000);
+  });
+
+  // Animate cards on scroll
+  function revealOnScroll() {
+    $('.card').each(function () {
+      var top_of_element = $(this).offset().top;
+      var bottom_of_screen = $(window).scrollTop() + $(window).height();
+
+      if (bottom_of_screen > top_of_element + 50) {
+        $(this).addClass('animated fadeInUp');
+      }
+    });
+  }
+
+  $(window).on('scroll', revealOnScroll);
+});
